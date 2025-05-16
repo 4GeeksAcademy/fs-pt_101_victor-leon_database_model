3acaf7fed4e8a3d60bf3d5f96a70897f4d5636a8
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-05-16 18:38:05 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,19 +1,65 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, ForeignKey, Enum
+import enum
 
 db = SQLAlchemy()
 
+class MediaType(enum.Enum):
+    image = "image"
+    video = "video"
+    reel = "reel"
+
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String, nullable=True)
+    lastname: Mapped[str] = mapped_column(String, nullable=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="author", cascade="all, delete")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="author", cascade="all, delete")
+    following: Mapped[list["Follower"]] = relationship("Follower", foreign_keys="[Follower.user_from_id]", back_populates="follower")
+    followers: Mapped[list["Follower"]] = relationship("Follower", foreign_keys="[Follower.user_to_id]", back_populates="followee")
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Post(db.Model):
+    __tablename__ = "post"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post", cascade="all, delete")
+    media: Mapped[list["Media"]] = relationship("Media", back_populates="post", cascade="all, delete")
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String, nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    author: Mapped["User"] = relationship("User", back_populates="comments")
+    post: Mapped["Post"] = relationship("Post", back_populates="comments")
+
+class Media(db.Model):
+    __tablename__ = "media"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[MediaType] = mapped_column(Enum(MediaType), nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    post: Mapped["Post"] = relationship("Post", back_populates="media")
+
+class Follower(db.Model):
+    __tablename__ = "follower"
+
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+
+    follower: Mapped["User"] = relationship("User", foreign_keys=[user_from_id], back_populates="following")
+    followee: Mapped["User"] = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
